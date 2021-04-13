fe8e178bc8fc5540d06822014213e185befd5fce
Alfonso Tienda <afoone@hotmail.com>
2021-04-13 19:52:06 +0200

    redirect

--- components/auth/AuthHOC.jsx
+++ components/auth/AuthHOC.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { useSession, getSession } from "next-auth/client";
+import { useRouter } from "next/router";
 
 const AuthHOC = ({ roles, children }) => {
   const [session, loading] = useSession();
+  const router = useRouter()
 
   console.log("roles", roles);
   console.log(loading, session);
   if (loading) return null;
 
-  if (!loading && !session) return <p>Access Denied</p>;
+  if (!loading && !session) {
+    router.push('/auth/signin?callbackurl=/immunization')
+    return <p>Access Denied</p>;
+  }
 
   return <div>{children}</div>;
 };
