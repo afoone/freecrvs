13edeedb4f72327ac26ade803799d0661e4bcf61
Alfonso Tienda <afoone@hotmail.com>
2021-04-15 18:19:55 +0200

    callback

--- components/auth/AuthHOC.jsx
+++ components/auth/AuthHOC.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import { useSession, getSession } from "next-auth/client";
 import { useRouter } from "next/router";
 
+
 const AuthHOC = ({ roles, children }) => {
   const [session, loading] = useSession();
-  const router = useRouter()
+  const router = useRouter();
 
   console.log("roles", roles);
   console.log(loading, session);
   if (loading) return null;
 
   if (!loading && !session) {
-    router.push('/auth/signin?callbackurl=/immunization')
+    router.push(`/auth/signin`);
     return <p>Access Denied</p>;
   }
 
