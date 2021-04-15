2c78a342962e696821929a4de5d3949023d9188a
Alfonso Tienda <afoone@hotmail.com>
2021-04-15 16:25:21 +0200

    removed comments

--- util/mongodb.js
+++ util/mongodb.js
@@ -2,18 +2,6 @@ import { MongoClient } from 'mongodb'
 
 const { MONGODB_URI, MONGODB_DB } = process.env
 
-// if (!MONGODB_URI) {
-//   throw new Error(
-//     'Please define the MONGODB_URI environment variable inside .env.local'
-//   )
-// }
-
-// if (!MONGODB_DB) {
-//   throw new Error(
-//     'Please define the MONGODB_DB environment variable inside .env.local'
-//   )
-// }
-
 /**
  * Global is used here to maintain a cached connection across hot reloads
  * in development. This prevents connections growing exponentially
