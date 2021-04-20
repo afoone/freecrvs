e82f10955b8df93505af688a2c30c6ea380362c9
Alfonso Tienda <afoone@hotmail.com>
2021-04-20 15:03:39 +0200

    remove strange behavihour on reload

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -10,85 +10,71 @@ const Layout = ({ children }) => {
   const [activeItem, setActiveItem] = useState("home");
   const router = useRouter();
 
-  useEffect(() => {
-    if (
-      typeof window !== "undefined" &&
-      "serviceWorker" in navigator &&
-      window.workbox !== undefined
-    ) {
-      console.log("workbox activating reload");
-      const wb = window.workbox;
-      // add event listeners to handle any of PWA lifecycle event
-      // https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-window.Workbox#events
-      wb.addEventListener("installed", (event) => {
-        console.log(`Event ${event.type} is triggered.`);
-        console.log(event);
-      });
+  // useEffect(() => {
+  //   if (
+  //     typeof window !== "undefined" &&
+  //     "serviceWorker" in navigator &&
+  //     window.workbox !== undefined
+  //   ) {
+  //     console.log("workbox activating reload");
+  //     const wb = window.workbox;
+  //     // add event listeners to handle any of PWA lifecycle event
+  //     // https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-window.Workbox#events
+  //     wb.addEventListener("installed", (event) => {
+  //       console.log(`Event ${event.type} is triggered.`);
+  //       console.log(event);
+  //     });
 
-      wb.addEventListener("controlling", (event) => {
-        console.log(`Event ${event.type} is triggered.`);
-        console.log(event);
-      });
+  //     wb.addEventListener("controlling", (event) => {
+  //       console.log(`Event ${event.type} is triggered.`);
+  //       console.log(event);
+  //     });
 
-      wb.addEventListener("activated", (event) => {
-        console.log(`Event ${event.type} is triggered.`);
-        console.log(event);
-      });
+  //     wb.addEventListener("activated", (event) => {
+  //       console.log(`Event ${event.type} is triggered.`);
+  //       console.log(event);
+  //     });
 
-      // A common UX pattern for progressive web apps is to show a banner when a service worker has updated and waiting to install.
-      // NOTE: MUST set skipWaiting to false in next.config.js pwa object
-      // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users
-      const promptNewVersionAvailable = (event) => {
-        // `event.wasWaitingBeforeRegister` will be false if this is the first time the updated service worker is waiting.
-        // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
-        // You may want to customize the UI prompt accordingly.
-        if (
-          confirm(
-            "A newer version of this web app is available, reload to update?"
-          )
-        ) {
-          wb.addEventListener("controlling", (event) => {
-            window.location.reload();
-          });
+  //     // A common UX pattern for progressive web apps is to show a banner when a service worker has updated and waiting to install.
+  //     // NOTE: MUST set skipWaiting to false in next.config.js pwa object
+  //     // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users
+  //     const promptNewVersionAvailable = (event) => {
+  //       // `event.wasWaitingBeforeRegister` will be false if this is the first time the updated service worker is waiting.
+  //       // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
+  //       // You may want to customize the UI prompt accordingly.
+  //       if (
+  //         confirm(
+  //           "A newer version of this web app is available, reload to update?"
+  //         )
+  //       ) {
+  //         wb.addEventListener("controlling", (event) => {
+  //           window.location.reload();
+  //         });
 
-          // Send a message to the waiting service worker, instructing it to activate.
-          wb.messageSW({ type: "SKIP_WAITING" });
-        } else {
-          console.log(
-            "User rejected to reload the web app, keep using old verion. New verion will be automatically load when user open the app next time."
-          );
-        }
-      };
+  //         // Send a message to the waiting service worker, instructing it to activate.
+  //         wb.messageSW({ type: "SKIP_WAITING" });
+  //       } else {
+  //         console.log(
+  //           "User rejected to reload the web app, keep using old verion. New verion will be automatically load when user open the app next time."
+  //         );
+  //       }
+  //     };
 
-      wb.addEventListener("waiting", promptNewVersionAvailable);
-      wb.addEventListener("externalwaiting", promptNewVersionAvailable);
+  //     wb.addEventListener("waiting", promptNewVersionAvailable);
+  //     wb.addEventListener("externalwaiting", promptNewVersionAvailable);
 
-      // ISSUE - this is not working as expected, why?
-      // I could only make message event listenser work when I manually add this listenser into sw.js file
-      wb.addEventListener("message", (event) => {
-        console.log(`Event ${event.type} is triggered.`);
-        console.log(event);
-      });
+  //     // ISSUE - this is not working as expected, why?
+  //     // I could only make message event listenser work when I manually add this listenser into sw.js file
+  //     wb.addEventListener("message", (event) => {
+  //       console.log(`Event ${event.type} is triggered.`);
+  //       console.log(event);
+  //     });
 
-      /*
-      wb.addEventListener('redundant', event => {
-        console.log(`Event ${event.type} is triggered.`)
-        console.log(event)
-      })
-      wb.addEventListener('externalinstalled', event => {
-        console.log(`Event ${event.type} is triggered.`)
-        console.log(event)
-      })
-      wb.addEventListener('externalactivated', event => {
-        console.log(`Event ${event.type} is triggered.`)
-        console.log(event)
-      })
-      */
 
-      // never forget to call register as auto register is turned off in next.config.js
-      wb.register();
-    }
-  }, []);
+  //     // never forget to call register as auto register is turned off in next.config.js
+  //     wb.register();
+  //   }
+  // }, []);
 
   const handleItemClick = (e, { name }) => {
     setActiveItem(name);
@@ -106,6 +92,8 @@ const Layout = ({ children }) => {
         break;
     }
   };
+
+
 
   return (
     <div className="layout">
