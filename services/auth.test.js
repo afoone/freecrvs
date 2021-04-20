867befe1b6d29dd71a528470f77ca8b23ed33292
Alfonso Tienda <afoone@hotmail.com>
2021-04-20 15:00:08 +0200

    remove console on test

--- services/auth.test.js
+++ services/auth.test.js
@@ -1,6 +1,5 @@
 import { passwordHash } from "./auth";
 
 test("hash password correctly", () => {
-  console.log(passwordHash("test"));
   expect(passwordHash("test")).toBe("cac76f43eb9243de03d52a0d12702d268fb101f50446af58a8c18985824c9587");
 });
