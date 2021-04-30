cfaee38dd6e24c30a3233e71d4f44230ef31d1a4
Alfonso Tienda <afoone@hotmail.com>
2021-04-30 09:15:29 +0200

    cors for vaccination passport

--- pages/api/patients/[id].js
+++ pages/api/patients/[id].js
@@ -1,10 +1,25 @@
-import {
-  add,
-  list,
-  deleteImmunization,
-  update,
-  get,
-} from "../../../services/immunization";
+import Cors from "cors";
+
+import { update, get } from "../../../services/immunization";
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 export default async (req, res) => {
   //   const { db } = await connectToDatabase();
@@ -17,6 +32,7 @@ export default async (req, res) => {
       break;
 
     default:
+      await runMiddleware(req, res, cors)
       const patientResponse = await get(id);
       res.json(patientResponse);
       break;
