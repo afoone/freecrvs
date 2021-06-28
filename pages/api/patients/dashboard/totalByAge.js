e6c7a6b541668a80a5ca6a4c47bb3b047a343dff
Alfonso Tienda <afoone@hotmail.com>
2021-06-28 16:42:47 +0200

    total by age improved

--- pages/api/patients/dashboard/totalByAge.js
+++ pages/api/patients/dashboard/totalByAge.js
@@ -2,41 +2,50 @@ import { getFirstDoseByAge } from "../../../../services/dashboard";
 export default async (req, res) => {
   const results = await getFirstDoseByAge();
 
+  const rangos = [
+    "<16",
+    "16-19",
+    "20-29",
+    "30-39",
+    "40-49",
+    "50-59",
+    "60-64",
+    ">=65",
+  ];
+
   const totals = results.reduce((acc, curr) => {
     const age = parseInt(curr._id.match(/[0-9]*/)[0]);
     if (!age) return acc;
-    console.log(age, curr);
     const newAcc = { ...acc };
-    console.log(parseInt(acc[age]), parseInt(curr.count));
     let range = "";
     switch (true) {
       case age < 16:
-        range = "<16";
+        range = rangos[0];
         break;
       case age <= 19:
-        range = "16-19";
+        range = rangos[1];
         break;
       case age <= 29:
-        range = "20-29";
+        range = rangos[2];
         break;
       case age <= 39:
-        range = "30-39";
+        range = rangos[3];
         break;
       case age <= 49:
-        range = "40-49";
+        range = rangos[4];
         break;
       case age <= 59:
-        range = "50-59";
+        range = rangos[5];
         break;
       case age <= 64:
-        range = "60-64";
+        range = rangos[6];
         break;
       default:
-        range = ">=65";
+        range = rangos[7];
         break;
     }
     newAcc[range] = (parseInt(acc[range]) || 0) + parseInt(curr.count);
     return newAcc;
   }, {});
-  res.json(totals);
+  res.json(rangos.map((rng) => ({ key: rng, value: totals[rng] })));
 };
