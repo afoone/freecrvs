545207fd5378ebf01f88bf700f6c7cffeabbff11
Alfonso Tienda <afoone@hotmail.com>
2021-07-05 12:49:25 +0200

    react-pdf-one

--- components/immunization/ImmunizationReport.jsx
+++ components/immunization/ImmunizationReport.jsx
@@ -1,84 +1,70 @@
-import React, { useRef, useState, useEffect } from "react";
-import { PDFExport } from "@progress/kendo-react-pdf";
-import { Button } from "@progress/kendo-react-buttons";
-import { Card, Icon, Image, Grid, Segment } from "semantic-ui-react";
-
-import axios from "axios";
+import React from "react";
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  Image,
+  Canvas,
+} from "@react-pdf/renderer";
 import PatientData from "../PatientData";
-const ImmunizationReport = ({ id }) => {
-  const pdfExportComponent = useRef(null);
-  const handleExport = (e) => {
-    pdfExportComponent.current.save();
-  };
+import QRCode from "qrcode.react";
 
-  const [patient, setPatient] = useState({});
+// Create styles
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "row",
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
+});
 
-  useEffect(() => {
-    axios.get(`/api/patients/${id}/`).then((res) => {
-      setPatient(res.data);
-    });
-  }, [id]);
+// Create Document Component
+const ImmunizationReport = ({ patient }) => (
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.section}>
+        <table >
+          <td>
+          <Text>Section #1</Text>
+          </td>
+          <td><Text>otra cosa</Text></td>
+        </table>
+        
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            width: "20%",
+            height: "20%",
+          }}
+        >
+          <Image src="/images/logo2.png" size="medium" />
 
-  console.log("patient", patient);
-
-  return (
-    <PDFExport ref={pdfExportComponent} paperSize="A4">
-      <div
-        className="button-area"
-        style={{ display: "flex", justifyContent: "flex-end" }}
-      >
-        <Button primary={true} onClick={handleExport}>
-          Download PDF
-        </Button>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "flex-start",
-          width: "20%",
-          height: "20%",
-        }}
-      >
-        <Image src="/images/logo2.png" size="medium" />
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        {patient && <PatientData patient={patient} />}
-      </div>
-
-      <Grid columns="equal">
-        <Grid.Column>
-          <Segment>Full Name:</Segment>
-          <Segment>Nationality:</Segment>
-          <Segment>Vaccine Name:</Segment>
-          <Segment>Date of Application:</Segment>
-          <Segment>Batch o:</Segment>
-          <Segment>Date of Birth:</Segment>
-          <Segment>Mobile Number:</Segment>
-          <Segment>NIN:</Segment>
-        </Grid.Column>
-        <Grid.Column>
-          <Segment>
-            {patient.firstName} {patient.middleName} {patient.lastName}
-          </Segment>
-          <Segment>{patient.nationality}</Segment>
-          {/* <Segment>{patient.vaccination.nameOfTheVaccine}</Segment> */}
-          <Segment>Date of Application:</Segment>
-          <Segment>Date of Application:</Segment>
-          <Segment>Date of Application:</Segment>
-          {/* <Segment>{patient.vaccination[0].batchNumber}</Segment> */}
-          <Segment>{patient.dateOfBirth}</Segment>
-          <Segment>{patient.phoneNumber}</Segment>
-          <Segment>{patient.NIN ? patient.NIN : "None"}</Segment>
-        </Grid.Column>
-      </Grid>
-    </PDFExport>
-  );
-};
+          {/* <Canvas width="200px" height="200px">
+            <QRCode value={patient._id} size={200} renderAs="canvas" />
+          </Canvas> */}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {/* {patient && <PatientData patient={patient} />} */}
+          <Text style={{ fontWeight: "bold" }}>{patient.firstName}</Text>
+        </div>
+      </View>
+      <View style={styles.section}>
+        <Text>Section #2</Text>
+      </View>
+    </Page>
+  </Document>
+);
 
 export default ImmunizationReport;
--- pages/immunization/[id]/report.jsx
+++ pages/immunization/[id]/report.jsx
@@ -1,15 +1,30 @@
-import React from "react";
-import ImmunizationLayout from "../../../components/immunization/Layout";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
-import ImmunizationReport from "../../../components/immunization/immunizationReport";
+import ImmunizationReport from "../../../components/immunization/ImmunizationReport";
+import { PDFViewer } from "@react-pdf/renderer";
+import axios from "axios";
+import { useState } from "react";
 
 const ImmunizationHome = () => {
   const { id } = useRouter().query;
 
+  const [patient, setPatient] = useState(undefined);
+
+  useEffect(() => {
+    id &&
+      axios.get(`/api/patients/${id}/`).then((res) => {
+        setPatient(res.data);
+      });
+  }, [id]);
+
   return (
-    <ImmunizationLayout>
-      {id && <ImmunizationReport id={id}></ImmunizationReport>}
-    </ImmunizationLayout>
+    <>
+      {patient && (
+        <PDFViewer width="100%" height="800px">
+          <ImmunizationReport patient={patient}></ImmunizationReport>
+        </PDFViewer>
+      )}
+    </>
   );
 };
 
