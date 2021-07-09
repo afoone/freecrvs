f9586a77791265e0b19b2df4def04577b7c0b207
Alfonso Tienda <afoone@hotmail.com>
2021-07-09 12:10:42 +0200

    fhir working

--- services/fhir.js
+++ services/fhir.js
@@ -1,55 +1,68 @@
-const identifierVaccineFirstDose = [{
-  vaccineCode: 91300,
-  vaccineName: "Pfizer-BioNTech",
-  vaccineAdministrationCode: "0001A"
-},{
-  vaccineCode: 91301,
-  vaccineName: "Moderna",
-  vaccineAdministrationCode: "0011A"
-},{vaccineCode: 91302,
-  vaccineName: "Astrazeneca",
-  vaccineAdministrationCode: "0021A"},
-  {vaccineCode: 91303,
-  vaccineName: "Jhonson&Jhonson - Jansen",
-  vaccineAdministrationCode: "0031A"},
-  {vaccineCode: 91304,
-  vaccineName: "NovaVax",
-  vaccineAdministrationCode: "0041A"}]
+const identifierVaccineFirstDose = [
+  {
+    vaccineCode: "91300",
+    vaccineName: "Pfizer-BioNTech",
+    vaccineAdministrationCode: "0001A",
+  },
+  {
+    vaccineCode: "91301",
+    vaccineName: "Moderna",
+    vaccineAdministrationCode: "0011A",
+  },
+  {
+    vaccineCode: "91302",
+    vaccineName: "Astrazeneca",
+    vaccineAdministrationCode: "0021A",
+  },
+  {
+    vaccineCode: "91303",
+    vaccineName: "Jhonson&Jhonson - Jansen",
+    vaccineAdministrationCode: "0031A",
+  },
+  {
+    vaccineCode: "91304",
+    vaccineName: "NovaVax",
+    vaccineAdministrationCode: "0041A",
+  },
+];
 
+const identifierVaccineSecondDose = [
+  {
+    vaccineCode: "91300",
+    vaccineName: "Pfizer-BioNTech",
+    vaccineAdministrationCode: "0002A",
+  },
+  {
+    vaccineCode: "91301",
+    vaccineName: "Moderna",
+    vaccineAdministrationCode: "0012A",
+  },
+  {
+    vaccineCode: "91302",
+    vaccineName: "Astrazeneca",
+    vaccineAdministrationCode: "0022A",
+  },
+  {
+    vaccineCode: "91304",
+    vaccineName: "NovaVax",
+    vaccineAdministrationCode: "0042A",
+  },
+];
 
-const identifierVaccineSecondDose = [{
-  vaccineCode: 91300,
-  vaccineName: "Pfizer-BioNTech",
-  vaccineAdministrationCode: "0002A"
-},{
-  vaccineCode: 91301,
-  vaccineName: "Moderna",
-  vaccineAdministrationCode: "0012A"
-},{vaccineCode: 91302,
-  vaccineName: "Astrazeneca",
-  vaccineAdministrationCode: "0022A"},
-  {vaccineCode: 91304,
-  vaccineName: "NovaVax",
-  vaccineAdministrationCode: "0042A"}]
-
-
-  const codeNumberFirstDose =()=>{
-    identifierVaccineFirstDose.map((i)=>{
-      if(i.vaccineName === patient.vaccination[0].nameOfTheVaccine){
-        return i.vaccineCode
-      }
-    })
-  }
-  const codeNumberSecondDose =()=>{
-    identifierVaccineFirstDose.map((i)=>{
-      if(i.vaccineName === patient.vaccination[1].nameOfTheVaccine){
-        return i.vaccineCode
-      }
-    })
-  }
+const codeNumberFirstDose = (nameOfTheVaccine) => {
+  return identifierVaccineFirstDose.filter((i) => {
+    if (i.vaccineName === nameOfTheVaccine) {
+      return i.vaccineCode;
+    }
+  })[0]?.vaccineCode;
+};
+export const codeNumberSecondDose = (nameOfTheVaccine) => {
+  return identifierVaccineSecondDose.filter(
+    (i) => i.vaccineName == nameOfTheVaccine
+  )[0]?.vaccineCode;
+};
 export const mapToFhir = (patient) => {
-  console.log("PATIENT", patient)
-  return {
+  const response = {
     resourceType: "Bundle",
     type: "collection",
     entry: [
@@ -61,88 +74,104 @@ export const mapToFhir = (patient) => {
             {
               family: patient.lastName,
               given: [patient.firstName, patient.middleName],
-            }, 
+            },
           ],
           identifier: {
-            use : "official",
-            system : "gambia-nin",
-            value : patient.NIN,
+            use: "official",
+            system: "gambia-nin",
+            value: patient.NIN,
           },
-          birthDate: patient.dateOfBirth
-        }
+          birthDate: patient.dateOfBirth,
+        },
       },
       {
         fullUrl: "resource:1",
         resource: {
           resourceType: "Immunization",
           meta: {
-            security: [{system: "https://smarthealth.cards/ial", code: "IAL2"}]
+            security: [
+              { system: "https://smarthealth.cards/ial", code: "IAL2" },
+            ],
           },
           status: "completed",
           vaccineCode: {
             coding: [
               {
                 system: "http://hl7.org/fhir/sid/cvx",
-                code: "207"
+                code: "207",
               },
               {
-                  system: "http://www.ama-assn.org/go/cpt",
-                  vaccineName:patient.vaccination[0].nameOfTheVaccine,
-                  code: codeNumberFirstDose
-              }
-            ]
+                system: "http://www.ama-assn.org/go/cpt",
+                vaccineName: patient.vaccination[0].nameOfTheVaccine,
+                code: codeNumberFirstDose(
+                  patient.vaccination[0].nameOfTheVaccine
+                ),
+              },
+            ],
           },
           patient: {
-            reference: "resource:0"
+            reference: "resource:0",
           },
           occurrenceDateTime: patient.vaccination[0]?.firstDoseDate,
           performer: [
             {
               actor: {
-                display: patient.vaccination[0].vaccinatorFullName
-              }
-            }
+                display: patient.vaccination[0].vaccinatorFullName,
+              },
+            },
           ],
-          lotNumber: patient.vaccination[0].batchNumber
-        }
+          lotNumber: patient.vaccination[0].batchNumber,
+        },
       },
-      (patient?.vaccination[1]?.nameOfTheVaccine)&&
-        
-        {
-          fullUrl: "resource:2",
-          resource: {
-            resourceType: "Immunization",
-            status: "completed",
-            vaccineCode: {
-              coding: [
-                {
-                  system: "http://hl7.org/fhir/sid/cvx",
-                  code: "207"
-                },
-                {
-                    system: "http://www.ama-assn.org/go/cpt",
-                    vaccineName:patient.vaccination[1].nameOfTheVaccine,
-                    code: codeNumberSecondDose
-                }
-              ]
+    ],
+  };
+
+  if (patient?.vaccination[1]?.nameOfTheVaccine) {
+    response.entry.push({
+      fullUrl: "resource:2",
+      resource: {
+        resourceType: "Immunization",
+        status: "completed",
+        meta: {
+          security: [
+            {
+              system: "https://smarthealth.cards/ial",
+              code: "IAL2",
             },
-            patient: {
-              reference: "resource:0"
+          ],
+        },
+        vaccineCode: {
+          coding: [
+            {
+              system: "http://hl7.org/fhir/sid/cvx",
+              code: "207",
             },
-            occurrenceDateTime: patient.vaccination[1]?.firstDoseDate,
-            performer: [
-              {
-                actor: {
-                  display: patient.vaccination[1].vaccinatorFullName
-                }
-              }
-            ],
-            lotNumber: patient.vaccination[1].batchNumber
-          }
-        
-      }
-    ]
+            {
+              system: "http://www.ama-assn.org/go/cpt",
+              vaccineName: patient.vaccination[1].nameOfTheVaccine,
+              code: codeNumberSecondDose(
+                patient.vaccination[1].nameOfTheVaccine
+              ),
+            },
+          ],
+        },
+        patient: {
+          reference: "resource:0",
+        },
+        occurrenceDateTime: patient.vaccination[1]?.firstDoseDate,
+        performer: [
+          {
+            actor: {
+              display: patient.vaccination[1].vaccinatorFullName,
+            },
+          },
+        ],
+        lotNumber: patient.vaccination[1].batchNumber,
+      },
+    });
   }
+
+  return response;
 };
 
 // para luego
--- services/fhir.test.js
+++ services/fhir.test.js
@@ -1,4 +1,4 @@
-import { mapToFhir, mapToPatient } from "./fhir";
+import { mapToFhir, mapToPatient, codeNumberSecondDose } from "./fhir";
 
 const patient = {
   _id: "608bfaf5a6f4f3001f90ce4c",
@@ -23,40 +23,205 @@ const patient = {
   patientPreviousAllergicReaction: "no",
   attendantAtBirth: "",
   placeOfDelivery: {
-      province: "79defb55-db0a-48a0-9a69-c539f3eedb7f",
-      district: "99222af6-0ba8-4da0-859f-2193eb16f4ac",
-      place: "BANJUL"
+    province: "79defb55-db0a-48a0-9a69-c539f3eedb7f",
+    district: "99222af6-0ba8-4da0-859f-2193eb16f4ac",
+    place: "BANJUL",
   },
   address: {
-      province: "79defb55-db0a-48a0-9a69-c539f3eedb7f",
-      district: "be4be157-0848-49c9-9816-5c0a2dbd9721",
-      address: "KOTU SOUTH"
+    province: "79defb55-db0a-48a0-9a69-c539f3eedb7f",
+    district: "be4be157-0848-49c9-9816-5c0a2dbd9721",
+    address: "KOTU SOUTH",
   },
   occupation: "",
   vaccination: [
-      {
-          firstDoseDate: "2021-04-07T12:38:31.000Z",
-          nameOfTheVaccine: "Astrazeneca",
-          batchNumber: "4120Z004",
-          serialNumber: "",
-          expirydate: "2021-04-13T12:38:31.000Z",
-          dateOfNextVisit: "2021-05-30T12:38:31.000Z",
-          vaccinatorFullName: "MUSA B ADRBOE",
-          aefi: [
-              {
-                  aefiSeverity: "severe",
-                  aefiDescription: ""
-              }
-          ],
-          placeofVaccination: {
-              facility: "",
-              province: "79defb55-db0a-48a0-9a69-c539f3eedb7f",
-              district: "76dd6213-4edd-46cf-a6c0-d3eb9dd172d0",
-              place: "TURNTABLE"
-          }
-      }
+    {
+      firstDoseDate: "2021-04-07T12:38:31.000Z",
+      nameOfTheVaccine: "Astrazeneca",
+      batchNumber: "4120Z004",
+      serialNumber: "",
+      expirydate: "2021-04-13T12:38:31.000Z",
+      dateOfNextVisit: "2021-05-30T12:38:31.000Z",
+      vaccinatorFullName: "MUSA B ADRBOE",
+      aefi: [
+        {
+          aefiSeverity: "severe",
+          aefiDescription: "",
+        },
+      ],
+      placeofVaccination: {
+        facility: "",
+        province: "79defb55-db0a-48a0-9a69-c539f3eedb7f",
+        district: "76dd6213-4edd-46cf-a6c0-d3eb9dd172d0",
+        place: "TURNTABLE",
+      },
+    },
   ],
   mother: {
+    firstName: "BINTOU",
+    middleName: "",
+    lastName: "NDURE",
+    dateOfBirth: null,
+    nationality: "GM",
+    NIN: "",
+    residentialAddress: {},
+    age: "",
+  },
+  father: {
+    firstName: "ASH MALICK",
+    middleName: "",
+    lastName: "NDOYE",
+    dateOfBirth: null,
+    nationality: "GM",
+    NIN: "",
+    residentialAddress: {},
+    age: "",
+  },
+};
+
+const expectedResponse = {
+  resourceType: "Bundle",
+  type: "collection",
+  entry: [
+    {
+      fullUrl: "resource:0",
+      resource: {
+        resourceType: "Patient",
+        name: [
+          {
+            family: "NDOYE",
+            given: ["AMIE", ""],
+          },
+        ],
+        identifier: {
+          use: "official",
+          system: "gambia-nin",
+          value: "181063-020-001",
+        },
+        birthDate: "1963-10-18T07:00:00.000Z",
+      },
+    },
+    {
+      fullUrl: "resource:1",
+      resource: {
+        resourceType: "Immunization",
+        meta: {
+          security: [
+            {
+              system: "https://smarthealth.cards/ial",
+              code: "IAL2",
+            },
+          ],
+        },
+        status: "completed",
+        vaccineCode: {
+          coding: [
+            {
+              system: "http://hl7.org/fhir/sid/cvx",
+              code: "207",
+            },
+            {
+                code: "91302",
+              system: "http://www.ama-assn.org/go/cpt",
+              vaccineName: "Astrazeneca",
+            },
+          ],
+        },
+        patient: {
+          reference: "resource:0",
+        },
+        occurrenceDateTime: "2021-04-07T12:38:31.000Z",
+        performer: [
+          {
+            actor: {
+              display: "MUSA B ADRBOE",
+            },
+          },
+        ],
+        lotNumber: "4120Z004",
+      },
+    },
+  ],
+};
+
+const patient2 = {
+    _id: "608bfaf5a6f4f3001f90ce4c",
+    patient: "cf41563d-2eb4-48d0-9d93-623b27668c37",
+    bornInGambia: true,
+    phoneNumber: "9896100",
+    firstName: "AMIE",
+    lastName: "NDOYE",
+    middleName: "",
+    baptismalName: "",
+    NIN: "181063-020-001",
+    age: "",
+    patientVaccineRegisterNumber: "116",
+    myChildId: "",
+    nationality: "GM",
+    gender: "F",
+    dateOfBirth: "1963-10-18T07:00:00.000Z",
+    placeOfWork: "HOUSE WIFE",
+    priorityGroups: [],
+    preexistingConditions: [],
+    previousCovid19Infection: null,
+    patientPreviousAllergicReaction: "no",
+    attendantAtBirth: "",
+    placeOfDelivery: {
+      province: "79defb55-db0a-48a0-9a69-c539f3eedb7f",
+      district: "99222af6-0ba8-4da0-859f-2193eb16f4ac",
+      place: "BANJUL",
+    },
+    address: {
+      province: "79defb55-db0a-48a0-9a69-c539f3eedb7f",
+      district: "be4be157-0848-49c9-9816-5c0a2dbd9721",
+      address: "KOTU SOUTH",
+    },
+    occupation: "",
+    vaccination: [
+      {
+        firstDoseDate: "2021-04-07T12:38:31.000Z",
+        nameOfTheVaccine: "Astrazeneca",
+        batchNumber: "4120Z004",
+        serialNumber: "",
+        expirydate: "2021-04-13T12:38:31.000Z",
+        dateOfNextVisit: "2021-05-30T12:38:31.000Z",
+        vaccinatorFullName: "MUSA B ADRBOE",
+        aefi: [
+          {
+            aefiSeverity: "severe",
+            aefiDescription: "",
+          },
+        ],
+        placeofVaccination: {
+          facility: "",
+          province: "79defb55-db0a-48a0-9a69-c539f3eedb7f",
+          district: "76dd6213-4edd-46cf-a6c0-d3eb9dd172d0",
+          place: "TURNTABLE",
+        },
+      },
+      {
+        firstDoseDate: "2021-04-07T12:38:31.000Z",
+        nameOfTheVaccine: "Astrazeneca",
+        batchNumber: "4120Z004",
+        serialNumber: "",
+        expirydate: "2021-04-13T12:38:31.000Z",
+        dateOfNextVisit: "2021-05-30T12:38:31.000Z",
+        vaccinatorFullName: "MUSA B ADRBOE",
+        aefi: [
+          {
+            aefiSeverity: "severe",
+            aefiDescription: "",
+          },
+        ],
+        placeofVaccination: {
+          facility: "",
+          province: "79defb55-db0a-48a0-9a69-c539f3eedb7f",
+          district: "76dd6213-4edd-46cf-a6c0-d3eb9dd172d0",
+          place: "TURNTABLE",
+        },
+      },
+
+    ],
+    mother: {
       firstName: "BINTOU",
       middleName: "",
       lastName: "NDURE",
@@ -64,9 +229,9 @@ const patient = {
       nationality: "GM",
       NIN: "",
       residentialAddress: {},
-      age: ""
-  },
-  father: {
+      age: "",
+    },
+    father: {
       firstName: "ASH MALICK",
       middleName: "",
       lastName: "NDOYE",
@@ -74,17 +239,121 @@ const patient = {
       nationality: "GM",
       NIN: "",
       residentialAddress: {},
-      age: ""
-  }
-}
-
-test("map to fhir patient", () => {
-  expect(mapToFhir(patient)).toEqual({
-      name: [
+      age: "",
+    },
+  };
+  
+  const expectedResponse2 = {
+    resourceType: "Bundle",
+    type: "collection",
+    entry: [
       {
-        family: "NDOYE",
-        given: ["AMIE",""],
+        fullUrl: "resource:0",
+        resource: {
+          resourceType: "Patient",
+          name: [
+            {
+              family: "NDOYE",
+              given: ["AMIE", ""],
+            },
+          ],
+          identifier: {
+            use: "official",
+            system: "gambia-nin",
+            value: "181063-020-001",
+          },
+          birthDate: "1963-10-18T07:00:00.000Z",
+        },
+      },
+      {
+        fullUrl: "resource:1",
+        resource: {
+          resourceType: "Immunization",
+          meta: {
+            security: [
+              {
+                system: "https://smarthealth.cards/ial",
+                code: "IAL2",
+              },
+            ],
+          },
+          status: "completed",
+          vaccineCode: {
+            coding: [
+              {
+                system: "http://hl7.org/fhir/sid/cvx",
+                code: "207",
+              },
+              {
+                  code: "91302",
+                system: "http://www.ama-assn.org/go/cpt",
+                vaccineName: "Astrazeneca",
+              },
+            ],
+          },
+          patient: {
+            reference: "resource:0",
+          },
+          occurrenceDateTime: "2021-04-07T12:38:31.000Z",
+          performer: [
+            {
+              actor: {
+                display: "MUSA B ADRBOE",
+              },
+            },
+          ],
+          lotNumber: "4120Z004",
+        },
+      },
+      {
+        fullUrl: "resource:2",
+        resource: {
+          resourceType: "Immunization",
+          meta: {
+            security: [
+              {
+                system: "https://smarthealth.cards/ial",
+                code: "IAL2",
+              },
+            ],
+          },
+          status: "completed",
+          vaccineCode: {
+            coding: [
+              {
+                system: "http://hl7.org/fhir/sid/cvx",
+                code: "207",
+              },
+              {
+                  code: "91302",
+                system: "http://www.ama-assn.org/go/cpt",
+                vaccineName: "Astrazeneca",
+              },
+            ],
+          },
+          patient: {
+            reference: "resource:0",
+          },
+          occurrenceDateTime: "2021-04-07T12:38:31.000Z",
+          performer: [
+            {
+              actor: {
+                display: "MUSA B ADRBOE",
+              },
+            },
+          ],
+          lotNumber: "4120Z004",
+        },
       },
     ],
-  });
+  };
+
+  test ("code number second dose works", () => {
+      expect(codeNumberSecondDose("Astrazeneca")).toBe("91302")
+  }) 
+
+
+test("map to fhir patient", () => {
+  expect(mapToFhir(patient)).toEqual(expectedResponse);
+  expect(mapToFhir(patient2)).toEqual(expectedResponse2)
 });
