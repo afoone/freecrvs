e4215cc890edbcb911a86040bee9dea1809d4e42
Alfonso Tienda <afoone@hotmail.com>
2021-10-01 12:30:49 +0200

    cors on checkpatient

--- pages/api/patients/checkPatient.js
+++ pages/api/patients/checkPatient.js
@@ -1,6 +1,27 @@
 import { getByNamesAndBirthDate } from "../../../services/immunization";
+import Cors from 'cors'
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
 
 export default async (req, res) => {
+  await runMiddleware(req, res, cors);
   const { firstName, lastName, birthDate } = req.query;
   const patient = await getByNamesAndBirthDate(firstName, lastName, birthDate);
   res.json(patient);
